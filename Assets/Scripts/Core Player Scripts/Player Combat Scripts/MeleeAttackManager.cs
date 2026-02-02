bc0bbc973983a724cbc722e117989dcd82529d5a
Luca Rampling <103632180+SushiMaster09@users.noreply.github.com>
2026-02-02 16:06:51 +0000

    full combat system

---
+++ Assets/Scripts/Core Player Scripts/Player Combat Scripts/MeleeAttackManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackManager : PlayerController
+{
+    [Header("Melee Attack Manager Stats")]
+    public float defaultForce = 300f;
+    public float upwardsForce = 600f;
+    public float movementTime = 0.1f;
+
+    private bool meleeAttack;
+
+    private Animator meleeAnimator;
+    private Animator anim;
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+        meleeAnimator = GetComponentInChildren<MeleeWeapon>().gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) { meleeAttack = true; }
+        else { meleeAttack = false; }
+
+        if (meleeAttack && Input.GetAxis("Vertical") > 0f)
+        {
+            anim.SetTrigger("UpwardMelee");
+            meleeAnimator.SetTrigger("UpwardMeleeSwipe");
+        }
+        if (meleeAttack && Input.GetAxis("Vertical") < 0f && !playerController.IsGrounded())
+        {
+            anim.SetTrigger("DownwardMelee");
+            meleeAnimator.SetTrigger("DownwardMeleeSwipe");
+        }
+        if ((meleeAttack && Input.GetAxis("Vertical") == 0) || meleeAttack && (Input.GetAxis("Vertical") < 0f && playerController.IsGrounded()))
+        {
+            anim.SetTrigger("ForwardMelee");
+
+            meleeAnimator.SetTrigger("ForwardMeleeSwipe");
+        }
+    }
+}
